d16f59345346304d6dd9e871eef7a2273a79c8c3
webdevyoo <yjk7454@naver.com>
2024-08-16 00:19:46 +0900

    feat: `Login.vue` - 컴포넌트 추가 및 로그인 폼 구성

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+  <v-main fill-height >
+    <v-layout align-center row wrap justify-center >
+      <v-flex xs3>
+        <v-card>
+          <v-toolbar flat dark height="30">
+            <v-toolbar-title>로그인</v-toolbar-title>
+          </v-toolbar>
+          <div class="pa-3">
+            <v-text-field
+              v-model="email"
+              label="이메일을 입력하세요"
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="password"
+              type="password"
+              label="패스워드를 입력하세요"
+            >
+          </v-text-field>
+          </div>
+        </v-card>
+      </v-flex>
+
+    </v-layout>
+  </v-main>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+    }
+  }
+}
+</script>
