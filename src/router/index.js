00c84ce36758db02927af9a5db574d38b9088a0e
webdevyoo <yjk7454@naver.com>
2024-08-17 22:53:58 +0900

    feat: `router/index.js` - AboutView제거, HomeView → Home, Login 라우터 등록

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "HomeView",
-    component: HomeView,
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "../views/Home.vue"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/login",
+    name: "login",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
   },
 ];
 
