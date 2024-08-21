6460e3e949e78fbbbd4a28482c2a51fc178283a8
webdevyoo <yjk7454@naver.com>
2024-08-22 00:02:26 +0900

    feat: `router/index.js` - 로그인 유무(store의 state값 접근)를 통한 router-guard 처리

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "../store";
 
 Vue.use(VueRouter);
 
+const rejectAuthUser = (to, from, next) => {
+  if(store.state.isLogin === true) {
+    alert("이미 로그인을 하였습니다.")
+    next("/") // 홈페이지로 이동
+  }
+  next() // router 입장 허용
+}
+const rejectOnlyAuth = (to, from, next) => {
+  if(store.state.isLogin === false) {
+    alert("로그인을 해주세요.")
+    next("/login") // 로그인 페이지로 이동
+  }
+  next() // router 입장 허용
+}
 const routes = [
   {
     path: "/",
@@ -13,12 +28,14 @@ const routes = [
   {
     path: "/login",
     name: "login",
+    beforeEnter: rejectAuthUser,
     component: () =>
       import(/* webpackChunkName: "login" */ "../views/Login.vue"),
   },
   {
     path: "/mypage",
     name: "mypage",
+    beforeEnter: rejectOnlyAuth,
     component: () =>
       import(/* webpackChunkName: "mypage" */ "../views/Mypage.vue"),
   },
